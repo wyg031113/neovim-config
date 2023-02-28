a1631adb96d19a237f4c77ef96f280b0f5e046c8
huozai <huozai.@pinduoduo.com>
2023-02-28 13:21:45 +0800

    my vim config

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("plugins.plugins-setup")
+
+require("core.options")
+
+-- 插件
+require("plugins.lualine")
+require("plugins/nvim-tree")
+require("plugins/treesitter")
+require("plugins/lsp")
+require("plugins/cmp")
+require("plugins/comment")
+require("plugins/autopairs")
+require("plugins/bufferline")
+require("plugins/gitsigns")
+require("plugins/telescope")
+require("plugins/telescope")
+require("plugins/coc-config")
+
+require("core.keymaps")
